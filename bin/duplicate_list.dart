6bad4fdbb72d30d6a7937df11ab6c216847f13ac
Desai Riddhi <desairiddhi15@gmail.com>
2022-04-04 10:03:51 +0530

    first commit

---
+++ bin/duplicate_list.dart
@@ -0,0 +1,15 @@
+import'dart:io';
+
+void main(){
+  List<int> array=[];
+  print("enter the size og array:");
+  int? n=int.parse(stdin.readLineSync()!);
+
+  print("enter the element of array:");
+  for(int i=0;i<n;++i){
+    int? size=int.parse(stdin.readLineSync()!);
+    array.add(size);
+  }
+  final uniqueNumber=array.toSet().toList();
+  print("after remove duplicate number:  \n$uniqueNumber");
+}
